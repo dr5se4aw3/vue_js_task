f797362c48d2b4560ff29ad5e5cfdda46b162335
hiroki matsubara <dr5se4aw3@yahoo.com>
2020-03-18 12:38:05 +0900

    コンポーネント練習

---
+++ task/main.js
@@ -0,0 +1,15 @@
+Vue.component('student-list',{
+  props: ['student'],
+  template: '<li>{{ student.name }}</li>'
+})
+
+var sample = new Vue({
+  el: '#student_list_area',
+  data: {
+    studentList: [
+      { id:0, name: '田中　太郎' },
+      { id:1, name: '吉田　兼行' },
+      { id:2, name: '松永　久秀' }
+    ]
+  }
+})
