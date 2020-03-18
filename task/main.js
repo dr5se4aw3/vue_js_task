d212c703209b540a1f3a993356eb6bdd57bc316d
hiroki matsubara <dr5se4aw3@yahoo.com>
2020-03-18 14:50:35 +0900

    Add 入力値をリストに表示させる

--- task/main.js
+++ task/main.js
@@ -1,20 +1,26 @@
+
+function hasEmpty(obj){
+  for(let i in obj){
+    if (obj[i] === ''){
+      return true
+    }
+  }
+  return false
+}
+
 Vue.component('student-list',{
   props: ['student'],
-  template: '<li>ID：{{student.id}}／名前：{{ student.name }}</li>'
+  template: '<li>ID：{{student.id}} ／ 名前：{{ student.name }} ／ コース：{{ student.course }} ／ 受講期：{{ student.season }}</li>'
 })
 
-var sampleList = new Vue({
+var students = new Vue({
   el: '#student_list_area',
   data: {
-    studentList: [
-      { id:0, name: '田中　太郎' },
-      { id:1, name: '吉田　兼行' },
-      { id:2, name: '松永　久秀' }
-    ]
+    list: []
   }
 })
 
-var studentProperty = new Vue({
+var student = new Vue({
   el: '#registration_form',
   data: {
     name: '',
@@ -26,11 +32,19 @@ var studentProperty = new Vue({
 var sampleButton = new Vue({
   el: '#push',
   data: {
-    counter: 0
+    lastStudentId: 0,
+    studentId: 0
   },
   methods: {
     addStudetList: function (event){
-      sampleList.studentList.push({id:3, name: studentProperty.name})
+      if (hasEmpty(student)){
+        window.alert('入力されていない値があります。');
+      }else{
+        if (students.list.length > 0) {
+          this.studentId = students.list[students.list.length-1].id + 1
+        }
+        students.list.push({id: this.studentId, name: student.name, course: student.course, season: student.season})
+      }
     }
   }
 })
